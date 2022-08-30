8ccc68d13473604ebac5ffb8d9561fb07606b649
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-30 23:55:08 +0700

    test2

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,14 +2,19 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
-  return weight/(height*height)
+  return (weight)/(height*height)
 }
 function getBMIMeaning(weight, height) {
   //code here
   let BMI = calculateBMI(getBMIMeaning)
-  let mean
-  if(BMI<18.5) return mean = "Underweight"
-  else if(18.<=BMI<=24.9) return mean = "Normal weight"
-  else return mean = "Overweight"
+  if(BMI<18.5) {
+    return "Underweight"
+  }
+  else if(18.<=BMI<=24.9) {
+    return "Normal weight"
+  }
+  else {
+    return "Overweight"
+  }
 }
 module.exports = getBMIMeaning
