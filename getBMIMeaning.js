be4dd66f83ab7e7a44e75feca31d744ff40a4175
Jairgun <jasda.jair9un@mail.kmutt.ac.th>
2022-08-31 00:15:38 +0700

    test7

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -9,6 +9,6 @@ function getBMIMeaning(weight, height) {
   let BMI = calculateBMI(weight,height)
   if(BMI < 18.5) {return "Underweight"}
   else if(18.5 <= BMI <= 24.9) {return "Normal weight"}
-  else if(BMI > 25.0){return "Overweight"}
+  else {return "Overweight"}
 }
 module.exports = getBMIMeaning
